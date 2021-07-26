0652fc2ef68f0ba04615d8dd49582f71be9cabea
Julia Surma <julia.surma12@gmail.com>
2021-07-26 19:44:30 +0100

    new vs of app - create forms to new users and schedules current status

--- source/index.js
+++ source/index.js
@@ -1,146 +1,132 @@
 const data = require("../app/data.js");
+const { users} = data; 
+const { schedules } = data; 
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const app = express();
+const path = require('path');
 const router = express.Router();
+ // listen requests
+ app.listen(3000);
+ var urlencodedParser = bodyParser.urlencoded({ extended: false }) // application/x-www-form-urlencoded
 
-app.get("/", function (req,res) {
-    res.send("Welcome to our schedule website");
-});
 
-app.get("/users/:id", function (req,res) {
-const id = Number(req.params.id);
-res.send(data.users[id]);
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/users', (req, res) => {
-    res.send(data.users)
-})
 
-// app.get('/users/:id', function (req,res) {
-//     const user = req.params.user;
-//     res.send("users", {user: data.users[0] });
+// before - task 3a
+// app.get("/", function (req,res) {
+//     res.send("Welcome to our schedule website");
 // });
 
-app.get('/schedules', (req, res) => {
- 
-    res.send(data.schedules);
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+// app.set('views', 'source');
+app.set('views', path.join(__dirname + '/views'));
+
+app.get('/',  (req, res) => {
+res.render("index",  { title: 'My Page', text: 'Welcome on schedule'});
     });
 
-// app.get("users/:id/schedules", (req,res) => {
-// const schedule = Number(req.params.schedule);
-// res.send(data.schedules[schedule]);
-// //             // res.send("schedules", {schedule: data.schedules[id]});
-// //             // res.send("schedules", {schedule: data.schedules[user_id]});
-// //             // res.send("schedules", {schedule: data.schedules[user_id]});
-// //             //  res.send("schedules", {schedule: data.schedules[0]});
-// //              res.send("schedules", {schedule: data.users.schedules[0]});
-
-//  });
-
-// app.get("/schedules/:schedule", function (req,res) {
-//   const schedule = req.params.schedule;
-//   res.send({schedule: data.schedules[schedule]});
-// });
-app.get('/users/:id/:schedules', (req, res) => {
-const id = Number(req.params.id);
-const schedules = data.schedules;
-const userSchedule = [];
-schedules.forEach((item) => { 
-if (item.user_id === id) {
- userSchedule.push(item);
-  }
- });
-  res.send(userSchedule);
- });
-
-app.listen(3000);
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false }) // application/x-www-form-urlencoded
-
-app.use(express.static('public'));
-
-// const {createHash} = require('crypto');
-
-// // lines: array of strings
-// function computeSHA256(lines) {
-//   const hash = createHash('sha256');
-//   for (let i = 0; i < lines.length; i++) {
-//     const line = lines[i].trim(); // remove leading/trailing whitespace
-//     if (line === '') continue; // skip empty lines
-//     hash.write(line); // write a single line to the buffer
-//   }
-
-//   return hash.digest('base64'); // returns hash as string
-// }
-
-const crypto = require('crypto');
-
-app.post('/users', urlencodedParser, function (req, res) {
-const hash = crypto.createHash('sha256').update(req.body.password).digest('base64');
-response = { 
-firstname:req.body.firstname,
-lastname:req.body.lastname,
-email: req.body.email,
-password: hash,
-};
-console.log(response);
-res.end(JSON.stringify(response));
-})
-
-app.post('/schedules', urlencodedParser, function (req, res) {
-response = {
-user_id: req.body.user_id,
-day: req.body.day,
-start_at: req.body.start_at,
-end_at: req.body.end_at,
-};
-console.log(response);
-res.end(JSON.stringify(response));
-})
-
-
-
-
-// // create application/json parser
-// var jsonParser = bodyParser.json()
- 
-// // // create application/x-www-form-urlencoded parser
-// var urlencodedParser = bodyParser.urlencoded({ extended: false })
- 
-// // // POST /login gets urlencoded bodies
-// app.post('/login', urlencodedParser, function (req, res) {
-// res.send('welcome, ' + req.body.username)
-//  })
- 
-// // // POST  gets JSON bodies
-// app.post('/schedules', jsonParser, function (req, res) {
-//  // create newschedules in req.body
-//  })
-
-
-
-// alert(ary[0]);
-// const module.exports.users  = [];
-// const firstname = data.users[0];
-// app.get('/users', (req, res) => {
-//  res.send(data.users);
-// });
-
- 
-
-
-// app.use(require('body-parser').urlencoded({ extended: false}));
-// app.post('/users', (req,res) => { 
-//     res.json({message: req.body})
+// before - 3a
+// app.get("/users/:id", function (req,res) {
+// const id = Number(req.params.id);
+// res.send(data.users[id]);
 // })
 
-    // const articleId = req.params.articleId;
-    // const post = posts[articleId];
-    // if(post === undefined) {
-    //     res.status(404).send(`Incorrect article id: ${articleId}`);
-    //     return;
-    // }
-    // res.render('singlePost', post)
-    // res.json({article: posts[articleId]})
+// get new user
+app.get('/users/new', (req, res) => {
+  res.render('NewUser', {title: 'Create new user', text: 'New User Form'});
+ });
 
+//get single user
+app.get('/users/:userId', (req, res) => {
+    const userId = Number(req.params.userId);
+    if (users[userId] === undefined) {
+      res.status(404).render('error', { title: 'Error'});
+    }
+    res.render('singleUser', { title: 'userId', userId, user: users[userId] });
+  });
+
+// before  3a
+// app.get('/users', (req, res) => {
+//     res.send(data.users)
+// })
+
+//get all users
+app.get('/users', (req, res) => {
+ res.render('users', {title : "All users", text: 'Users List', users})
+});
+
+//before 3a
+// app.get('/schedules', (req, res) => {
+//     res.send(data.schedules);
+//     });
+
+
+// get new schedule
+app.get('/schedules/new', (req, res) => {
+  res.render('NewSchedule', {title: 'Create new schedule', text: 'New Schedule Form'});
+ });
+
+
+// get all schedules
+app.get('/schedules', (req, res) => {
+    res.render('schedules', { title: 'Schedules', text: 'Check all schedules', schedules });
+        });
+
+// get schedule per user
+app.get('/users/:userId/schedules', (req, res) => {
+  const userId = req.params.userId;
+  const userSchedule = [];
+  for (let i = 0; i < schedules.length; i++) {
+    if (schedules[i].user_id === Number(userId)) {
+      userSchedule.push(schedules[i]);
+    }
+  }
+  res.render('singleSchedulesUser',  {title: 'ScheduleforspecificUser', text: 'Schedule User', users, userId, user: users[userId], schedules, userSchedule});
+});
+
+const crypto = require('crypto');
+app.post('/users', urlencodedParser, function (req, res) {
+  const userNew = req.body;
+  // const hash = crypto.createHash('sha256').update(req.body.password).digest('base64');
+  //  const userNew = { 
+  // firstname: firstname.req.body, 
+  // lastname: lastname.req.body,
+  // email: email.req.body,
+  // password: password.req.body,
+  // };
+  userNew.password = crypto.createHash('sha256').update(req.body.password).digest('base64');
+  users.push(userNew);
+  res.redirect('users');
+});
+
+
+//before - 3a task
+// app.post('/users', urlencodedParser, function (req, res) {
+// const hash = crypto.createHash('sha256').update(req.body.password).digest('base64');
+// response = { 
+// firstname:req.body.firstname,
+// lastname:req.body.lastname,
+// email: req.body.email,
+// password: hash,
+// };
+// console.log(response);
+// res.end(JSON.stringify(response));
+// })
+
+app.post('/schedules', urlencodedParser, function (req, res) {
+  const NewSchedule = req.body;
+// response = {
+// user_id: req.body.user_id,
+// day: req.body.day,
+// start_at: req.body.start_at,
+// end_at: req.body.end_at,
+// };
+schedules.push(NewSchedule)
+res.redirect('schedules');
+})
